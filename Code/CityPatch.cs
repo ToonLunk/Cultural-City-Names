a921bd174e998513d27a51e7d850d24a9c689bc0
Toon Lunk <tylerchasebrock@gmail.com>
2023-07-28 14:25:54 -0400

    update code to create names.json
    since the file can't be included, just create it

--- Code/CityPatch.cs
+++ Code/CityPatch.cs
@@ -1,19 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using HarmonyLib;
-using Newtonsoft.Json.Linq;
-using UnityEngine;
-using UnityEngine.Profiling;
-using ReflectionUtility;
-using System.Text.RegularExpressions;
 using System.IO;
-using Newtonsoft;
+using HarmonyLib;
 using Newtonsoft.Json;
-using ai.behaviours;
+using ReflectionUtility;
+using UnityEngine;
 
 namespace CulturalCityNames
 {
@@ -58,24 +49,49 @@ namespace CulturalCityNames
                 raceTemplateName = "NONE";
             }
 
-            string jsonPath = "./names.json"; // Assuming the JSON file is in the same directory as the mod's DLL.
-            string json;
+            // Create a list of CityTitle objects (same as the original JSON)
+            List<CityTitle> cityTitles = new List<CityTitle>
+            {
+                new CityTitle { ending = "ian", type = "prefix", extension = "City of " },
+                new CityTitle { ending = "dia", type = "prefix", extension = "Ankh-" },
+                new CityTitle { ending = "hin", type = "suffix", extension = " Prefecture" },
+                new CityTitle { ending = "ese", type = "prefix", extension = "Province of " },
+                new CityTitle { ending = "lia", type = "prefix", extension = "Holy " },
+                new CityTitle { ending = "lic", type = "prefix", extension = "State of " },
+                new CityTitle { ending = "jen", type = "prefix", extension = "Jarldom of " },
+                new CityTitle { ending = "bio", type = "suffix", extension = " Zone" },
+                new CityTitle { ending = "deb", type = "suffix", extension = " Lands" },
+                new CityTitle { ending = "den", type = "suffix", extension = " Precinct" },
+                new CityTitle { ending = "ana", type = "suffix", extension = " Parish" },
+                new CityTitle { ending = "tsk", type = "suffix", extension = " Canton" },
+                new CityTitle { ending = "kin", type = "suffix", extension = " Oblast" },
+                new CityTitle { ending = "ova", type = "prefix", extension = "County of " },
+                new CityTitle { ending = "gin", type = "prefix", extension = "Duchy of " },
+                new CityTitle { ending = "for", type = "prefix", extension = "Barony of " },
+                new CityTitle { ending = "esk", type = "prefix", extension = "Township of " },
+                new CityTitle { ending = "ink", type = "prefix", extension = "Village of " },
+                new CityTitle { ending = "bia", type = "prefix", extension = "Fiefdom of " },
+                new CityTitle { ending = "ask", type = "suffix", extension = " Place" }
+            };
 
+            // Convert the list to JSON
+            string json = JsonConvert.SerializeObject(cityTitles, Formatting.Indented);
+
+            // Save the JSON data to names.json file in the .modconfig folder
+            string modConfigPath = Path.Combine(Application.dataPath, "../.modconfig");
+            Directory.CreateDirectory(modConfigPath); // Create the .modconfig folder if it doesn't exist
+            string filePath = Path.Combine(modConfigPath, "names.json");
             try
             {
-                using (System.IO.StreamReader reader = new System.IO.StreamReader(jsonPath))
-                {
-                    json = reader.ReadToEnd();
-                }
+                File.WriteAllText(filePath, json);
             }
             catch (Exception e)
             {
-                Debug.LogError("Error reading names.json: " + e.Message);
-                return false; // Return false to prevent mod from changing city name if JSON cannot be read.
+                Debug.LogError("Error writing names.json: " + e.Message);
+                return false;
             }
 
-            var cityTitles = JsonConvert.DeserializeObject<List<CityTitle>>(json);
-
+            // Apply city name modifications using the generated names.json data
             string suffix = "";
             string prefix = "";
 
