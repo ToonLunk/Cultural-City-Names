ef6043c9665f2c7fe6300f1a13c4ec84793dd3f4
Toon Lunk <tylerchasebrock@gmail.com>
2023-07-28 21:00:09 -0400

    move all JSON creation logic to Main.cs

--- Code/CityPatch.cs
+++ Code/CityPatch.cs
@@ -20,35 +20,6 @@ namespace CulturalCityNames
     {
         static bool Prefix(City __instance)
         {
-            string infoMessage = "<INFO>Cultural City Names: ";
-
-            var kingdom = (Kingdom)Reflection.GetField(__instance.GetType(), __instance, "kingdom");
-            string kingdomName = kingdom.name.ToString();
-
-            string cultureName;
-            try
-            {
-                Culture culture = kingdom.getCulture();
-                cultureName = culture.data.name.ToString();
-            }
-            catch
-            {
-                Debug.Log(infoMessage + "No culture found for new city; must be a capital?");
-                cultureName = "NONE";
-            }
-
-            string raceTemplateName;
-            try
-            {
-                Race race = (Race)Reflection.GetField(__instance.GetType(), __instance, "race");
-                raceTemplateName = race.name_template_city.ToString();
-            }
-            catch
-            {
-                Debug.Log(infoMessage + "No race found for new city!");
-                raceTemplateName = "NONE";
-            }
-
             // Check if names.json file exists in .modconfig folder
             string modConfigPath = Path.Combine(Application.dataPath, "../mods/.modconfig");
             string filePath = Path.Combine(modConfigPath, "names.json");
@@ -70,45 +41,8 @@ namespace CulturalCityNames
             }
             else
             {
-                // If the file doesn't exist, create a new list of CityTitle objects (same as the original JSON)
-                cityTitles = new List<CityTitle>
-                {
-                    new CityTitle { ending = "ian", type = "prefix", extension = "City of " },
-                    new CityTitle { ending = "dia", type = "prefix", extension = "Ankh-" },
-                    new CityTitle { ending = "hin", type = "suffix", extension = " Prefecture" },
-                    new CityTitle { ending = "ese", type = "prefix", extension = "Province of " },
-                    new CityTitle { ending = "lia", type = "prefix", extension = "Holy " },
-                    new CityTitle { ending = "lic", type = "prefix", extension = "State of " },
-                    new CityTitle { ending = "jen", type = "prefix", extension = "Jarldom of " },
-                    new CityTitle { ending = "bio", type = "suffix", extension = " Zone" },
-                    new CityTitle { ending = "deb", type = "suffix", extension = " Lands" },
-                    new CityTitle { ending = "den", type = "suffix", extension = " Precinct" },
-                    new CityTitle { ending = "ana", type = "suffix", extension = " Parish" },
-                    new CityTitle { ending = "tsk", type = "suffix", extension = " Canton" },
-                    new CityTitle { ending = "kin", type = "suffix", extension = " Oblast" },
-                    new CityTitle { ending = "ova", type = "prefix", extension = "County of " },
-                    new CityTitle { ending = "gin", type = "prefix", extension = "Duchy of " },
-                    new CityTitle { ending = "for", type = "prefix", extension = "Barony of " },
-                    new CityTitle { ending = "esk", type = "prefix", extension = "Township of " },
-                    new CityTitle { ending = "ink", type = "prefix", extension = "Village of " },
-                    new CityTitle { ending = "bia", type = "prefix", extension = "Fiefdom of " },
-                    new CityTitle { ending = "ask", type = "suffix", extension = " Place" }
-                };
-
-                // Convert the list to JSON
-                string json = JsonConvert.SerializeObject(cityTitles, Formatting.Indented);
-
-                // Save the JSON data to names.json file in the .modconfig folder
-                Directory.CreateDirectory(modConfigPath); // Create the .modconfig folder if it doesn't exist
-                try
-                {
-                    File.WriteAllText(filePath, json);
-                }
-                catch (Exception e)
-                {
-                    Debug.LogError("Error writing names.json: " + e.Message);
-                    return false;
-                }
+                Debug.LogError("names.json file not found. Make sure it exists in .modconfig folder.");
+                return false; // Return false to prevent mod from changing city name if names.json is missing.
             }
 
             // Apply city name modifications using the data from names.json
@@ -135,4 +69,3 @@ namespace CulturalCityNames
         }
     }
 }
-
